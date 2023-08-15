473c50659421ec83f2a1da3531958f74920eb58e
wsafight <984292420@qq.com>
2023-08-15 22:07:01 +0800

    #FEAT: add todo

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,9 @@
-export default function () {
-  return 'hello world';
-}
+/**
+ * TODO 这里需要考虑 node_modules 不存在的问题
+ */
+
+const dynamicImport = (path: string) => import(path);
+
+export { dynamicImport };
+
+export default dynamicImport;
